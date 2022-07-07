e13586e4cdb88a29ac0c648bdd51e14fcac8b419
Richard Pascual <ricpascual@gmail.com>
2022-07-06 22:42:13 -0500

    añadiendo enventos de javasscripts a nuestro codigo

---
+++ aprender.js
@@ -0,0 +1,33 @@
+const compra = {tipo:"Online", modeol:"Carro", color: "blanco"}
+compra.dueño = "Richard" //añadir nuevo, o cambiar el diccionario
+
+console.log(compra) // imprimir en la consola
+
+//operadores de javascript
+
+let lenght = 16
+let LastName = "Huallpa"
+let x = {firstNmae: "Richard", LastName: "Yapo"}
+
+console.log(LastName)
+// let p1 = 1
+// let p2 = 3 
+
+function miPrimerFunction(p1, p2){
+    return p1 + p2
+}
+
+console.log(miPrimerFunction(3,4))
+
+// obejtos lusgo aprender programación orientado a objectos 
+
+const car = {
+    tipo: "toyota", 
+    modelo: "hilux", 
+    año: 2000, 
+    dueño: "Ricardo"
+}
+
+console.log(car.dueño) // imprimiendo en pantalla en la consla con diccionarios
+console.log(car["dueño"]) // otra forma de accerde a un diccionario
+
--- static/js/app.js
+++ static/js/app.js
@@ -1,27 +1,40 @@
-const compra = {tipo:"Online", modeol:"Carro", color: "blanco"}
-compra.dueño = "Richard" //añadir nuevo, o cambiar el diccionario
-
-console.log(compra) // imprimir en la consola
-
-//operadores de javascript
-
-let lenght = 16
-let LastName = "Huallpa"
-let x = {firstNmae: "Richard", LastName: "Yapo"}
-
-console.log(LastName)
-// let p1 = 1
-// let p2 = 3 
-
-function miPrimerFunction(p1, p2){
-    return p1 + p2
+function suma(p1, p2){
+    console.log(p1 + p2)
+    console.log("como estamos muchachos jajajaja")
 }
 
-console.log(miPrimerFunction(3,4))
 
-// obejtos
 
-const car = {tipo: "toyota", modelo: "hilux", año: 2000, dueño: "Ricardo"}
+// const compra = {tipo:"Online", modeol:"Carro", color: "blanco"}
+// compra.dueño = "Richard" //añadir nuevo, o cambiar el diccionario
 
-console.log(car)
+// console.log(compra) // imprimir en la consola
+
+// //operadores de javascript
+
+// let lenght = 16
+// let LastName = "Huallpa"
+// let x = {firstNmae: "Richard", LastName: "Yapo"}
+
+// console.log(LastName)
+// // let p1 = 1
+// // let p2 = 3 
+
+// function miPrimerFunction(p1, p2){
+//     return p1 + p2
+// }
+
+// console.log(miPrimerFunction(3,4))
+
+// // obejtos lusgo aprender programación orientado a objectos 
+
+// const car = {
+//     tipo: "toyota", 
+//     modelo: "hilux", 
+//     año: 2000, 
+//     dueño: "Ricardo"
+// }
+
+// console.log(car.dueño) // imprimiendo en pantalla en la consla con diccionarios
+// console.log(car["dueño"]) // otra forma de accerde a un diccionario
 
