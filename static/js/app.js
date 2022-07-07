3d8ab509a0782b6b6f7ee2148e10628385e742a7
Richard Pascual <ricpascual@gmail.com>
2022-07-06 22:11:42 -0500

    el segundo avance en javascrip

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,27 @@
-//valor indefinido
-// var x
-// console.log(x)
+const compra = {tipo:"Online", modeol:"Carro", color: "blanco"}
+compra.dueño = "Richard" //añadir nuevo, o cambiar el diccionario
 
-var x = "5"
-var y = "5"
+console.log(compra) // imprimir en la consola
 
-let total = x + y;
-console.log(total)
+//operadores de javascript
+
+let lenght = 16
+let LastName = "Huallpa"
+let x = {firstNmae: "Richard", LastName: "Yapo"}
+
+console.log(LastName)
+// let p1 = 1
+// let p2 = 3 
+
+function miPrimerFunction(p1, p2){
+    return p1 + p2
+}
+
+console.log(miPrimerFunction(3,4))
+
+// obejtos
+
+const car = {tipo: "toyota", modelo: "hilux", año: 2000, dueño: "Ricardo"}
+
+console.log(car)
+
